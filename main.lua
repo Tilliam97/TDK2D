8a799ea09acbf6e4616a59b0a769dba16116500b
William Tibbs III <wtibbs3@gmail.com>
2025-06-18 07:06:53 -0400

    added working character sprite and animations need to tidy them up a few stray pixels

--- main.lua
+++ main.lua
@@ -1,32 +1,48 @@
 function love.load()
   anim8 = require 'libraries/anim8'
+  love.graphics.setDefaultFilter("nearest", "nearest")
 
   player = {}
   player.x = 400
   player.y = 200
   player.speed = 3
   --player.sprite = love.graphics.sprite
-  player.spriteSheet = love.graphics.newImage('sprites/Swordsman.png')
+  player.spriteSheet = love.graphics.newImage('sprites/rogue.png')
+  player.grid = anim8.newGrid(32, 35, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
+  player.animations = {}
+  player.animations.up = anim8.newAnimation(player.grid('1-3', 4), 0.22)
+  player.animations.down = anim8.newAnimation(player.grid('1-3', 1), 0.22)
+  player.animations.left = anim8.newAnimation(player.grid('1-3', 2), 0.22)
+  player.animations.right = anim8.newAnimation(player.grid('1-3', 3), 0.22) 
+
+  player.anim = player.animations.left
+
   background = love.graphics.newImage('sprites/background.png')
-  player.grid = anim8.newGrid(24, 21, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
 end 
 
 function love.update(dt)
   if love.keyboard.isDown("right") then 
     player.x = player.x + player.speed
+    player.anim = player.animations.right
   end
   if love.keyboard.isDown("left") then 
     player.x = player.x - player.speed
+    player.anim = player.animations.left
   end
   if love.keyboard.isDown("up") then 
     player.y = player.y - player.speed
+    player.anim = player.animations.up
   end
   if love.keyboard.isDown("down") then 
     player.y = player.y + player.speed
+    player.anim = player.animations.down
   end
+
+  player.anim:update(dt)
 end
 
 function love.draw()
-  love.graphics.circle("fill", player.x, player.y, 90)
+  love.graphics.draw(background, 0, 0)
+  player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3)
   -- love.window.setFullscreen(true) makes the game fullscreen
 end
---
+++ player.lua
@@ -0,0 +1,12 @@
+--player = nil
+
+function makePlayer()
+  -- local p = {}
+  -- p.x = 400
+  -- p.y = 200
+  -- p.speed = 3
+  -- --p.sprite = love.graphics.sprite
+  -- p.spriteSheet = love.graphics.newImage('sprites/rogue.png')
+  -- p.grid = anim8.newGrid(24, 21, p.spriteSheet:getWidth(), p.spriteSheet:getHeight())
+  -- return p
+end
