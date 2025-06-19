1c119593ddd0b68e3117febf945e5ce6b9303385
William Tibbs III <wtibbs3@gmail.com>
2025-06-18 23:07:15 -0400

    added updated sprite with no stray pixels for better rendering

--- main.lua
+++ main.lua
@@ -7,42 +7,61 @@ function love.load()
   player.y = 200
   player.speed = 3
   --player.sprite = love.graphics.sprite
-  player.spriteSheet = love.graphics.newImage('sprites/rogue.png')
-  player.grid = anim8.newGrid(32, 37, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0, 0, 1)
+  --player.spriteSheet = love.graphics.newImage('sprites/rogue_sat.png')
+  player.spriteSheet = love.graphics.newImage('sprites/rogue_sat.png')
+  --player.grid = anim8.newGrid(325, 380, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0, 0, 6)
+  player.grid = anim8.newGrid(42, 48, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0, 0, 1.2)
+
   player.animations = {}
   player.animations.up = anim8.newAnimation(player.grid('1-3', 4), 0.20)
   player.animations.down = anim8.newAnimation(player.grid('1-3', 1), 0.20)
   player.animations.left = anim8.newAnimation(player.grid('1-3', 2), 0.20)
   player.animations.right = anim8.newAnimation(player.grid('1-3', 3), 0.20) 
 
-  player.anim = player.animations.left
+  player.anim = player.animations.down
 
   background = love.graphics.newImage('sprites/background.png')
+  width = love.graphics.getWidth()
+  height = love.graphics.getHeight()
 end 
 
 function love.update(dt)
+  local isMoving = false
+
   if love.keyboard.isDown("right") then 
+    isMoving = true
     player.x = player.x + player.speed
     player.anim = player.animations.right
   end
-  if love.keyboard.isDown("left") then 
+  if love.keyboard.isDown("left") then
+    isMoving = true 
     player.x = player.x - player.speed
     player.anim = player.animations.left
   end
   if love.keyboard.isDown("up") then 
+    isMoving = true
     player.y = player.y - player.speed
     player.anim = player.animations.up
   end
   if love.keyboard.isDown("down") then 
+    isMoving = true
     player.y = player.y + player.speed
     player.anim = player.animations.down
+  end
+  if isMoving == false then
+    player.anim:gotoFrame(2)
   end
 
   player.anim:update(dt)
 end
 
 function love.draw()
-  love.graphics.draw(background, 0, 0)
-  player.anim:draw(player.spriteSheet, player.x, player.y, nil, 3)
+    for y = 0, height - 1, background:getHeight() do
+      for x = 0, width -1, background:getWidth() do
+      love.graphics.draw(background, x, y)
+    end
+  end
+  --love.graphics.draw(background, 0, 0)
+  player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2)
   -- love.window.setFullscreen(true) makes the game fullscreen
 end
