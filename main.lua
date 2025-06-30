b10bb4757f0f911ed42dc1f61163466797160cef
William Tibbs III <wtibbs3@gmail.com>
2025-06-30 16:31:32 -0400

    added collision to all objects in scene and corrected draw order to be able to walk behind trees

--- main.lua
+++ main.lua
@@ -5,10 +5,80 @@ function love.load()
   camera = require 'libraries/camera'
   cam = camera()
 
-  --anim8 = require 'libraries/anim8'
+  --anim8 = require 'libraries/anim8' MOVED TO Player
 
   sti = require 'libraries/sti'
   gameMap = sti('maps/Tutorial_map.lua')
+
+  world:addCollisionClass('World')
+  world:addCollisionClass('Player')
+
+  bounds = {}
+  --vertices = {}
+  if gameMap.layers["Tree_Bounds"] then
+    for i, obj in pairs(gameMap.layers["Tree_Bounds"].objects) do
+      local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height) 
+      wall:setType('static')
+      wall:setCollisionClass('World'--[[,{ignores = {'Player'}}]])
+      table.insert(bounds, wall)
+    end
+  end
+
+  if gameMap.layers["Cliff_Bounds"] then
+      for j, obj in pairs(gameMap.layers["Cliff_Bounds"].objects) do
+        if obj.shape == "rectangle" then
+          local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+          wall:setType('static')
+          table.insert(bounds, wall)
+          wall:setCollisionClass('World')
+        end
+      end
+    end
+
+    -- if gameMap.layers["Cliff_Bounds"] then
+    --   for j, obj in ipairs( gameMap.layers["Cliff_Bounds"].objects) do
+    --     if obj.polygon and (obj.name == "cliff_1" or obj.name == "cliff_2") then
+    --       local vertices = {}
+    --       for k, point in pairs(obj.polygon) do
+    --         table.insert(vertices, point.x)
+    --         table.insert(vertices, point.y)
+    --       end
+    --       local cliff = world:newPolygonCollider(vertices)
+    --        cliff:setType('static')
+           --table.insert(bounds, vertices)
+
+          -- elseif obj.polygon and obj.name == "cliff_2" then
+          --   --local vertices_2 = {}
+          --   for k, point in pairs(obj.polygon) do
+          --     table.insert(vertices, point.x)
+          --     table.insert(vertices, point.y)
+          -- end
+          -- local cliff = world:newPolygonCollider(vertices)
+          --  cliff:setType('static')
+            --local cliff = world:newPolygonCollider(vertices)
+    --     end
+    --   end
+    -- end
+
+    if gameMap.layers["Camp_Bounds"] then
+      for l, obj in pairs(gameMap.layers["Camp_Bounds"].objects) do
+        if obj.ellipse then
+          --Convert to circle 
+          local centerX = obj.x + obj.width / 2
+          local centerY = obj.y + obj.height / 2
+          local diameter = (67.6923 + 42.3077) / 2
+
+          local camp = world:newCircleCollider(centerX, centerY, diameter/2) 
+          camp:setType('static')
+          table.insert(bounds, camp)
+          camp:setCollisionClass('World')
+        end
+      end
+    end
+
+    
+
+    --world:draw() --draws debug collisions
 
   love.graphics.setDefaultFilter("nearest", "nearest")
 
@@ -16,56 +86,13 @@ function love.load()
   height = love.graphics.getHeight()
 
   player = Player:new(--[[player.x]]width*0.5, --[[player.y]]height*0.5, --[[speed]]250)
-  -- player = {}
-  -- player.x = width / 2
-  -- player.y = height / 2
-  -- player.speed = 500
-  -- --player.sprite = love.graphics.sprite
-  -- --player.spriteSheet = love.graphics.newImage('sprites/rogue_sat.png')
-  -- player.spriteSheet = love.graphics.newImage('sprites/rogue_sat.png')
-  -- --player.grid = anim8.newGrid(325, 380, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0, 0, 6)
-  -- player.grid = anim8.newGrid(42, 48, player.spriteSheet:getWidth(), player.spriteSheet:getHeight(), 0, 0, 1.2)
-
-  -- player.animations = {}
-  -- player.animations.up = anim8.newAnimation(player.grid('1-3', 4), 0.20)
-  -- player.animations.down = anim8.newAnimation(player.grid('1-3', 1), 0.20)
-  -- player.animations.left = anim8.newAnimation(player.grid('1-3', 2), 0.20)
-  -- player.animations.right = anim8.newAnimation(player.grid('1-3', 3), 0.20) 
-
-  -- player.anim = player.animations.down
-
+ 
   --background = love.graphics.newImage('sprites/background.png')
 end 
 
 function love.update(dt)
   player:update(dt)
-  -- local isMoving = false
-
-  -- if love.keyboard.isDown("right") then 
-  --   isMoving = true
-  --   player.x = player.x + player.speed
-  --   player.anim = player.animations.right
-  -- end
-  -- if love.keyboard.isDown("left") then
-  --   isMoving = true 
-  --   player.x = player.x - player.speed
-  --   player.anim = player.animations.left
-  -- end
-  -- if love.keyboard.isDown("up") then 
-  --   isMoving = true
-  --   player.y = player.y - player.speed
-  --   player.anim = player.animations.up
-  -- end
-  -- if love.keyboard.isDown("down") then 
-  --   isMoving = true
-  --   player.y = player.y + player.speed
-  --   player.anim = player.animations.down
-  -- end
-  -- if isMoving == false then
-  --   player.anim:gotoFrame(2)
-  -- end
-
-  -- player.anim:update(dt)
+  
 
   cam:lookAt(player.x, player.y)
   
@@ -101,37 +128,29 @@ function love.draw()
 -- for k,v in pairs(gameMap.layers) do
 --   love.graphics.print(string.format("%s (%s)", k, v.type), 10, y)
 --   y = y + 20
-  --debug.debug()
+--   --debug.debug()
 -- end
 
+
   cam:attach()
-    --   for y = 0, height - 1, background:getHeight() do  [[loops over an image to display it as the background over the entire window size]]
+    --   for y = 0, height - 1, background:getHeight() do         [[loops over an image to display it as the background over the entire window size]] --debugging logic 
     --     for x = 0, width -1, background:getWidth() do
     --     love.graphics.draw(background, x, y)
     --   end
     -- end
-    -- gameMap:drawLayer(gameMap.layers["Ground"])
-    -- gameMap:drawLayer(gameMap.layers["Trees.Trees_1"])
-    -- gameMap:drawLayer(gameMap.layers["Trees.Trees_2"])
-    -- gameMap:drawLayer(gameMap.layers["Trees.Trees_3"])     starter Map
-    -- gameMap:drawLayer(gameMap.layers["Trees.Trees_4"])
-    -- gameMap:drawLayer(gameMap.layers["Trees.Trees_5"])
-    -- gameMap:drawLayer(gameMap.layers["Shrubs"])
-    -- gameMap:drawLayer(gameMap.layers["Shrubs_2"])
-    -- gameMap:drawLayer(gameMap.layers["Camp"])
-    -- gameMap:drawLayer(gameMap.layers["Bridge"])
+    
 
     gameMap:drawLayer(gameMap.layers["Ground"])
     gameMap:drawLayer(gameMap.layers["Shade"])
-    player:draw()
-    gameMap:drawLayer(gameMap.layers["Trees.Trees_1"])
     gameMap:drawLayer(gameMap.layers["Trees.Shade"])
     gameMap:drawLayer(gameMap.layers["Trees.Grass"])
     gameMap:drawLayer(gameMap.layers["Cliff"])
-    gameMap:drawLayer(gameMap.layers["Cliff_Shade"])
+    --gameMap:drawLayer(gameMap.layers["Cliff_Shade"])
     gameMap:drawLayer(gameMap.layers["Camp"])
     gameMap:drawLayer(gameMap.layers["Bridge_Exit"])
     gameMap:drawLayer(gameMap.layers["Cobble_Exit"])
+    player:draw()
+    gameMap:drawLayer(gameMap.layers["Trees.Trees_1"])
     --love.graphics.draw(background, 0, 0)
     --player.anim:draw(player.spriteSheet, player.x, player.y, nil, 2, nil, --[[offset x , offset y]] 21, 24)
     -- love.window.setFullscreen(true) makes the game fullscreen
